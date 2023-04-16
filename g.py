6c6213736d6af188827d45b8b7f1a1c0ff652535
Tofuwawa <jimdreamdream@gmail.com>
2023-04-16 12:08:34 +0800

    second version

--- g.py
+++ g.py
@@ -1,2 +1,16 @@
 import random
 #guess num
+r = random.randint(1,100)
+times = 0
+while True:
+    times += 1
+    ans = int(input("Please guess the number"))
+    #ans =int(ans)
+    if ans == r:
+        print("You are right")
+        break
+    elif ans > r:
+        print("Too much, please try again")
+    elif ans < r:
+        print("Fewer,plase try again")
+print("for",times,"times")
