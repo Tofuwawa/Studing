1f0f1fa9be76b373902218f090041ca0cb1ae7d5
Tofuwawa <jimdreamdream@gmail.com>
2023-04-20 22:22:12 +0800

    fun ver

--- list00.py
+++ list00.py
@@ -1,32 +1,56 @@
 import os
-products=[]
-#test file exist status
-if os.path.isfile("products.csv"):
-    print("Yes, the file is here")
-    with open("products.csv", "r") as f: # read file
+def check_file(filename): #read file
+    products=[]
+    if os.path.isfile(filename): #test file exist status
+        print("Yes, the file is here")
+    else:
+        print("No found file")
+    return products
+
+def read_file(filename):
+    products = []
+    with open(filename, "r") as f:  # read file
         for line in f:
             if "商品,價格" in line:
                 continue
             name, price = line.strip().split(",")  # 從左而右，先strip()把前後空格與換行符號\n除掉，然後slipt(",")把line以逗號為分隔
             products.append([name, price])
     print(products)
-else:
-    print("No found file")
+    return products
 
-# user enter
-while True:
-    name = input("Please enter the product")
-    if name == "q":
-        break
-    price = input("Please enter the price")
-    products.append([name,price])
+def user_input(products):# user enter
+    while True:
+        name = input("Please enter the product")
+        if name == "q":
+            break
+        price = input("Please enter the price")
+        price = int(price)
+        products.append([name,price])
+    return products
 
-# with open("products.csv", "w", encoding="utf-8") as f:
-with open("products.csv", "w") as f:  #utf-8編號可以解決中文亂碼現象
-    f.write("商品,價格\n")
-    # f.write("products,price\n")
+def print_products(products):
     for p in products:
-        f.write(f"{p[0]},{p[1]}\n")
+        print(f"{p[0]}'s price is {p[1]}")
+
+def write_file(filename,products):# with open("products.csv", "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:  #utf-8編號可以解決中文亂碼現象
+        f.write("商品,價格\n")
+        # f.write("products,price\n")
+        for p in products:
+            f.write(f"{p[0]},{p[1]}\n")
+
+def main():
+    filename = input("Please enter the file name")
+    if os.path.isfile(filename) :
+        print("Yes, the file is here")
+        products = read_file("products.csv")
+        products = user_input(products)
+        print_products(products)
+        write_file(filename, products)
+    else:
+        print("No found file")
+
+main()
 
 
 
